aa849906424aec35181bd55715f58a48d64e0e74
DogeThe1st <129316508+DogeThe1st@users.noreply.github.com>
2023-03-30 18:20:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var width = 1280;
 var height = 720;
+let screenwidth = 1280;
+let screenheight = 720;
 var prompt = "https://bing.com"
 document.getElementById("myFrame").height = height;
 document.getElementById("myFrame").width = width
 
 function Submit() {
-	prompt = "https://" + document.getElementById("URL").value;
-  console.log("Going to " + prompt)
+  prompt = "https://" + document.getElementById("URL").value;
+  //console.log("Going to " + prompt)
   document.getElementById("myFrame").src = prompt;
 }
